45a87b04d395a69fd372ddfa9b2271620aea37b9
Siza <laurenadhikari16@gmail.com>
2021-04-19 15:34:30 +0545

    folders' structure upto date

---
+++ frontend/reserve-n-dine/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Typography} from '@material-ui/core'; // this is where we import the necessary stuffs from the material ui core for designing the pages 
+
+
+const App =() =>{
+    return (
+        <div>
+            <Typography variant ='h1'> Hi there!</Typography>
+        </div>
+    );
+}
+
+export default App;
